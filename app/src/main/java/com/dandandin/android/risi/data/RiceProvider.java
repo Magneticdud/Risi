3328ee592844dc3dcd61a2a0c7ddf6345a7220df
Magnetic_dud <Magneticdud@users.noreply.github.com>
2018-01-07 16:09:38 +0100

    more comments

--- app/src/main/java/com/dandandin/android/risi/data/RiceProvider.java
+++ app/src/main/java/com/dandandin/android/risi/data/RiceProvider.java
@@ -10,32 +10,38 @@ public class RiceProvider extends ContentProvider {
     /** Database helper object */
     private RiceDbHelper mDbHelper;
 
+    //Initialize the provider and the database helper object.
     @Override
     public boolean onCreate() {
         mDbHelper = new RiceDbHelper(getContext());
         return true;
     }
 
+    //Perform the query for the given URI. Use the given projection, selection, selection arguments, and sort order.
     @Override
     public Cursor query(Uri uri, String[] strings, String s, String[] strings1, String s1) {
         return null;
     }
 
+    //Returns the MIME type of data for the content URI.
     @Override
     public String getType(Uri uri) {
         return null;
     }
 
+    //Insert new data into the provider with the given ContentValues.
     @Override
     public Uri insert(Uri uri, ContentValues contentValues) {
         return null;
     }
 
+    //Delete the data at the given selection and selection arguments.
     @Override
     public int delete(Uri uri, String s, String[] strings) {
         return 0;
     }
 
+    //Updates the data at the given selection and selection arguments, with the new ContentValues.
     @Override
     public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
         return 0;
