701024e396f7cc1c2d2177012c472ef61403bb0f
Magnetic_dud <Magneticdud@users.noreply.github.com>
2018-01-12 22:26:42 +0100

    placeholder

--- app/src/main/java/com/dandandin/android/risi/data/RiceProvider.java
+++ app/src/main/java/com/dandandin/android/risi/data/RiceProvider.java
@@ -1,10 +1,13 @@
 package com.dandandin.android.risi.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import com.dandandin.android.risi.data.RiceContract.RiceEntry;
 
 public class RiceProvider extends ContentProvider {
     /** URI matcher code for the content URI for the rice table */
@@ -53,8 +56,44 @@ public class RiceProvider extends ContentProvider {
 
     //Perform the query for the given URI. Use the given projection, selection, selection arguments, and sort order.
     @Override
-    public Cursor query(Uri uri, String[] strings, String s, String[] strings1, String s1) {
-        return null;
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
+                        String sortOrder) {
+        // Get readable database
+        SQLiteDatabase database = mDbHelper.getReadableDatabase();
+
+        // This cursor will hold the result of the query
+        Cursor cursor = null;
+
+        // Figure out if the URI matcher can match the URI to a specific code
+        int match = sUriMatcher.match(uri);
+        switch (match) {
+            case RICES:
+                // For the RICES code, query the risi table directly with the given
+                // projection, selection, selection arguments, and sort order. The cursor
+                // could contain multiple rows of the risi table.
+                // TODO: Perform database query on risi table
+                break;
+            case RICE_ID:
+                // For the RICE_ID code, extract out the ID from the URI.
+                // For an example URI such as "content://com.example.android.pets/pets/3",
+                // the selection will be "_id=?" and the selection argument will be a
+                // String array containing the actual ID of 3 in this case.
+                //
+                // For every "?" in the selection, we need to have an element in the selection
+                // arguments that will fill in the "?". Since we have 1 question mark in the
+                // selection, we have 1 String in the selection arguments' String array.
+                selection = RiceEntry._ID + "=?";
+                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
+
+                // This will perform a query on the pets table where the _id equals 3 to return a
+                // Cursor containing that row of the table.
+                cursor = database.query(RiceEntry.TABLE_NAME, projection, selection, selectionArgs,
+                        null, null, sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot query unknown URI " + uri);
+        }
+        return cursor;
     }
 
     //Returns the MIME type of data for the content URI.
