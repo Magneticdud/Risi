dd1097703a3d1a80da84f7120a001a00d53cde5f
Magnetic_dud <Magneticdud@users.noreply.github.com>
2018-01-12 23:16:36 +0100

    lets use it

--- app/src/main/java/com/dandandin/android/risi/CatalogActivity.java
+++ app/src/main/java/com/dandandin/android/risi/CatalogActivity.java
@@ -72,8 +72,6 @@ public class CatalogActivity extends AppCompatActivity {
      * the pets database.
      */
     private void displayDatabaseInfo() {
-        // Create and/or open a database to read from it
-        SQLiteDatabase db = mDbHelper.getReadableDatabase();
 
         // Define a projection that specifies which columns from the database
         // you will actually use after this query.
@@ -84,15 +82,14 @@ public class CatalogActivity extends AppCompatActivity {
                 RiceEntry.COLUMN_PACKAGING,
                 RiceEntry.COLUMN_PRICE };
 
-        // Perform a query on the pets table
-        Cursor cursor = db.query(
-                RiceEntry.TABLE_NAME,   // The table to query
-                projection,            // The columns to return
-                null,                  // The columns for the WHERE clause
-                null,                  // The values for the WHERE clause
-                null,                  // Don't group the rows
-                null,                  // Don't filter by row groups
-                null);                   // The sort order
+        // Perform a query on the provider using the ContentResolver.
+        // Use the {@link RicEntry#CONTENT_URI} to access the rice data.
+        Cursor cursor = getContentResolver().query(
+                RiceEntry.CONTENT_URI,  // The content URI of the words table
+                projection,             // The columns to return for each row
+                null,           // Selection criteria
+                null,       // Selection criteria
+                null);          // The sort order for the returned rows
 
         TextView displayView = (TextView) findViewById(R.id.text_view_rice);
 
