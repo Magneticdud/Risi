691cbf2a7b7ea55336a155545b56b40864c690e2
Magnetic_dud <Magneticdud@users.noreply.github.com>
2018-01-14 00:04:33 +0100

    invalidateOptionsMenu()

--- app/src/main/java/com/dandandin/android/risi/EditorActivity.java
+++ app/src/main/java/com/dandandin/android/risi/EditorActivity.java
@@ -95,6 +95,9 @@ public class EditorActivity extends AppCompatActivity implements
         if (mCurrentRiceUri == null) {
             // This is a new rice, so change the app bar to say "Add a Rice"
             setTitle(getString(R.string.editor_activity_title_new_rice));
+            // Invalidate the options menu, so the "Delete" menu option can be hidden
+            // It doesn't make sense to delete a rice that hasn't been created yet.)
+            invalidateOptionsMenu();
         } else {
             // Otherwise this is an existing rice, so change app bar to say "Edit Rice"
             setTitle(getString(R.string.editor_activity_title_edit_rice));
@@ -232,6 +235,19 @@ public class EditorActivity extends AppCompatActivity implements
         // Inflate the menu options from the res/menu/menu_editor.xml file.
         // This adds menu items to the app bar.
         getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    /** * This method is called after invalidateOptionsMenu(), so that the
+     * menu can be updated (some menu items can be hidden or made visible).*/
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        // If this is a new rice, hide the "Delete" menu item.
+        if (mCurrentRiceUri == null) {
+            MenuItem menuItem = menu.findItem(R.id.action_delete);
+            menuItem.setVisible(false);
+        }
         return true;
     }
 
