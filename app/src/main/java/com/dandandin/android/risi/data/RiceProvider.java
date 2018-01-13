ec807e066b0fcc04e2e0116cb9b4350325c89d70
Magnetic_dud <Magneticdud@users.noreply.github.com>
2018-01-13 19:11:38 +0100

    delete()

--- app/src/main/java/com/dandandin/android/risi/data/RiceProvider.java
+++ app/src/main/java/com/dandandin/android/risi/data/RiceProvider.java
@@ -160,10 +160,23 @@ public class RiceProvider extends ContentProvider {
         return ContentUris.withAppendedId(uri, id);
     }
 
-    //Delete the data at the given selection and selection arguments.
-    @Override
-    public int delete(Uri uri, String s, String[] strings) {
-        return 0;
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        // Get writeable database
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case RICES:
+                // Delete all rows that match the selection and selection args
+                return database.delete(RiceEntry.TABLE_NAME, selection, selectionArgs);
+            case RICE_ID:
+                // Delete a single row given by the ID in the URI
+                selection = RiceEntry._ID + "=?";
+                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
+                return database.delete(RiceEntry.TABLE_NAME, selection, selectionArgs);
+            default:
+                throw new IllegalArgumentException("Deletion is not supported for " + uri);
+        }
     }
 
     //Updates the data at the given selection and selection arguments, with the new ContentValues.
